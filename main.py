73ccb05d9704886db2ef31efd250544609dcb435
Shai A. Bennathan <giftgiverson@gmail.com>
2022-02-08 10:14:06 +0200

    lint fixes - main

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+"""
+Entry point for QRescue automation
+"""
+
 from matches import update_matched_remove_unmatched, squash_matched
 from duplicates import update_and_remove_duplicates
 
 
 def update(is_last=False):
+    """
+    Update match indices, remove irrelevant files
+    :param is_last: (default='False' Should the last available folder be used
+    """
     from_id = update_matched_remove_unmatched(is_last)
     squash_matched()
     update_and_remove_duplicates(from_id)
@@ -11,14 +19,17 @@ def update(is_last=False):
 
 if __name__ == '__main__':
     # === PHASE 2: Index matching, and remove irrelevant, PhotoRec recovered files
-    # Use while PhotoRec is still running, to track status and reduce the total disc space needed for this process
+    # Use while PhotoRec is still running, to track status
+    # and reduce the total disc space needed for this process
     update()
     # Use if PhotoRec has finished running
     # update(True)
 
     # === PHASE 3: Recovering single-matched files 'blindly'
+    # pylint: disable=fixme
     # TODO
 
     # === PHASE 4: Recovering multi-matched files interactively, by type
+    # pylint: disable=fixme
     # TODO
     print('Done and done')
