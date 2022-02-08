8b8a5e7e3d47c191e5f98a3156332b08f9a35e0e
Shai A. Bennathan <giftgiverson@gmail.com>
2022-02-08 06:44:28 +0200

    main documentation

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import affected
 from matches import update_matched_remove_unmatched, squash_matched
 from duplicates import update_and_remove_duplicates
 
@@ -11,5 +10,15 @@ def update(is_last=False):
 
 
 if __name__ == '__main__':
+    # === PHASE 2: Index matching, and remove irrelevant, PhotoRec recovered files
+    # Use while PhotoRec is still running, to track status and reduce the total disc space needed for this process
     update()
+    # Use if PhotoRec has finished running
+    # update(True)
+
+    # === PHASE 3: Recovering single-matched files 'blindly'
+    # TODO
+
+    # === PHASE 4: Recovering multi-matched files interactively, by type
+    # TODO
     print('Done and done')
