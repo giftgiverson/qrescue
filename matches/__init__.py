4a80556af754f892e332490ccb980692ea22ca45
Shai A. Bennathan <giftgiverson@gmail.com>
2022-02-08 09:59:02 +0200

    lint fixes - matches module

--- matches/__init__.py
+++ matches/__init__.py
@@ -1,3 +1,7 @@
+"""
+Match rescued files to affected files
+"""
+
 from .matches import update_matched_remove_unmatched, load_matches, squash_matched, encode_match
 
 __all__ = ['update_matched_remove_unmatched', 'load_matches', 'squash_matched', 'encode_match']
--- matches/matches.py
+++ matches/matches.py
@@ -1,15 +1,25 @@
-from my_env import rescue_share, data_file, rescue_folder, rescue_folder_prefix, data_backup
+"""
+Implement mathcing rescued files to affected files
+"""
+
 from os.path import join as pjoin, exists, getsize
 from os import listdir, remove
 from re import search as regex_match
+from my_env import RESCUE_FOLDER, data_file, rescue_folder, RESCUE_FOLDER_PREFIX, data_backup
 from affected import load_ext
 
 
+# pylint: disable=too-few-public-methods
 class _RecupScanner:
     def __init__(self):
         self.ext_histogram, self.ext_names = load_ext()
 
     def scan_recup(self, id_from, id_to):
+        """
+        Scan recovered folders for matches, reporting to matched and unmatched CSV files
+        :param id_from: Start of range of folder IDs to scan (inclusive)
+        :param id_to: End of range of folder IDs to scan (inclusive)
+        """
         m_size = 0
         u_size = 0
         with data_file('matched.csv', 'a') as matched_file:
@@ -19,7 +29,8 @@ class _RecupScanner:
                         m_s, u_s = self._scan_recup_dir(folder_id, matched_file, unmatched_file)
                         m_size += m_s
                         u_size += u_s
-                        report = ', '.join(['recup_dir.' + str(folder_id), str(m_s), str(u_s)]) + '\n'
+                        report =\
+                            ', '.join([rescue_folder(str(folder_id)), str(m_s), str(u_s)]) + '\n'
                         print(report)
                         report_file.write(report)
         print(f'== Matched {m_size / u_size * 100:.2f}%,'
@@ -44,7 +55,10 @@ class _RecupScanner:
                         match_ext_names.append(ext)
                         match_count += self.ext_histogram[ext][f_size]
             report = \
-                ', '.join([str(match_count), '|'.join(match_ext_names), str(f_size), str(folder_id), f_name]) + '\n'
+                ', '.join(
+                    [str(match_count), '|'.join(match_ext_names),
+                     str(f_size), str(folder_id), f_name])\
+                + '\n'
             if match_count:
                 matched_file.write(report)
                 m_size += f_size
@@ -58,14 +72,21 @@ def _parse_match(line):
     parts = line.split(',')
     return \
         int(parts[0]), parts[1].strip(), int(parts[2]), \
-        [tuple([p.strip() for p in parts[n * 2 - 1:n * 2 + 1]]) for n in range(2, 1 + len(parts) >> 1)]
+        [tuple(p.strip() for p in parts[n * 2 - 1:n * 2 + 1])
+         for n in range(2, 1 + len(parts) >> 1)]
 
 
-# matches, array of tuple(count, |-delimited extensions, size, array of tuple (recup_dir_id, file name))
 def load_matches(match_type):
+    """
+    Load match objects from file
+    :param match_type: match file prefix
+    :return: array of tuple(
+        count, |-delimited extensions, size,
+        array of tuple (recup_dir_id, file name))
+    """
     matches = []
-    with data_file(match_type + 'matched.csv') as f:
-        for line in f:
+    with data_file(match_type + 'matched.csv') as file:
+        for line in file:
             matches.append(_parse_match(line))
     return matches
 
@@ -73,8 +94,8 @@ def load_matches(match_type):
 def _remove_unmatched(unmatched):
     last_id = -1
     cur_dir = ''
-    for um in unmatched:
-        cur_id, f_name = um[3][0]
+    for non_matching in unmatched:
+        cur_id, f_name = non_matching[3][0]
         if last_id != cur_id:
             last_id = cur_id
             cur_dir = rescue_folder(cur_id)
@@ -89,14 +110,21 @@ def _remove_unmatched(unmatched):
 
 def _detect_next_range():
     to_id = max([int(m.group(1))
-                 for m in [regex_match(rescue_folder_prefix + r'\.(\d+)', f)
-                           for f in listdir(rescue_share)] if m]
+                 for m in [regex_match(RESCUE_FOLDER_PREFIX + r'\.(\d+)', f)
+                           for f in listdir(RESCUE_FOLDER)] if m]
                 ) - 1
     from_id = max([int(m[3][0][0]) for m in load_matches('')]) + 1
     return from_id, to_id
 
 
 def update_matched_remove_unmatched(is_last=False):
+    """
+    Scan new rescued files, match to affected files by extension and size, and delete rescued
+     files which don't match any affected file
+    :param is_last: (optional, default=False) Did PhotoRec finish running, so we can safely
+     include the last recovery folder?
+    :return: ID of first folder with new files
+    """
     from_id, to_id = _detect_next_range()
     if is_last:
         to_id += 1
@@ -122,6 +150,11 @@ def _squash_matches(matches):
 
 
 def encode_match(match):
+    """
+    Encodes a match object to string
+    :param match: match
+    :return: string encoding match
+    """
     return ', '.join([str(v) for v in match[:3]] + [a for b in match[3] for a in b]) + '\n'
 
 
@@ -137,6 +170,9 @@ def _sort_squashed(squashed):
 
 
 def squash_matched():
+    """
+    Squashes matches by detecting single-matches, and grouping multi-matches by affected
+    """
     matches = load_matches('')
     squashed = _squash_matches(matches)
     _sort_squashed(squashed)
