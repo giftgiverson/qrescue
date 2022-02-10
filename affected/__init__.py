b1c4a326f199ed1470bcc6ac804875d589e409b4
Shai A. Bennathan <giftgiverson@gmail.com>
2022-02-10 16:23:30 +0200

    refactor affected files and folders as classes

--- affected/__init__.py
+++ affected/__init__.py
@@ -2,6 +2,6 @@
 Handles affected files
 """
 
-from .affected import load_affected, load_ext, update_files
+from .affected import load_files, load_folders, load_ext, update_files
 
-__all__ = ['load_affected', 'load_ext', 'update_files']
+__all__ = ['load_files', 'load_folders', 'load_ext', 'update_files']
