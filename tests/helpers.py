528588a714c7e252e16d19b0239fab0000c7ae3a
Shai A. Bennathan <giftgiverson@gmail.com>
2022-02-14 04:55:16 +0200

    reuse keyed-class mock
    Signed-off-by: Shai A. Bennathan <giftgiverson@gmail.com>

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,6 +3,9 @@ Test helpers
 """
 
 import pytest
+from mock import PropertyMock
+
+from my_misc import static_vars
 
 
 @pytest.fixture
@@ -12,3 +15,21 @@ def mocker_file_read_lines(request, mocker):
     lines = ('\n'.join(lines_marker.args) + '\n') if lines_marker else ''
     mocked_folder_file_data = mocker.mock_open(read_data=lines)
     return mocker.patch('builtins.open', mocked_folder_file_data)
+
+
+# pylint: disable=no-member
+@static_vars(key=0)
+def next_key():
+    """provides an increasing key for each call"""
+    next_key.key += 1
+    return next_key.key
+
+
+@pytest.fixture
+def mocker_keyed_class(request, mocker):
+    """overrides <class>__init__ and provides an increasing key for each call to <class>.key"""
+    class_name = request.node.get_closest_marker('class_name').args[0]
+    init_mocked = mocker.patch(class_name + '.__init__', return_value=None)
+    mocked = mocker.patch(class_name + '.key', new_callable=PropertyMock)
+    mocked.side_effect = next_key
+    return init_mocked
--- tests/test_affected_folders.py
+++ tests/test_affected_folders.py
@@ -2,34 +2,11 @@
 Tests for affected.folders
 """
 import pytest
-from mock import PropertyMock, call
+from mock import call
 
 from affected.folders import AffectedFolder, load_folders
-from my_misc import static_vars
 # pylint: disable=unused-import
-from .helpers import mocker_file_read_lines
-
-
-# region mocks
-
-# pylint: disable=no-member
-@static_vars(key=0)
-def next_key():
-    """provides an increasing key for each call"""
-    next_key.key += 1
-    return next_key.key
-
-
-@pytest.fixture
-def mocker_affected_folder(mocker):
-    """provides an increasing key for each call to AffectedFolder.key"""
-    af_init_mocked = mocker.patch('affected.folders.AffectedFolder.__init__', return_value=None)
-    af_mocked = mocker.patch('affected.folders.AffectedFolder.key', new_callable=PropertyMock)
-    af_mocked.side_effect = next_key
-    return af_init_mocked
-
-
-# endregion mocks
+from .helpers import mocker_file_read_lines, next_key, mocker_keyed_class
 
 
 # region tests
@@ -50,19 +27,21 @@ def test_affected_folder_class():
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-argument
 @pytest.mark.file_lines('line1', 'line2', 'line3')
-def test_load_folder(mocker_file_read_lines, mocker_affected_folder):
+@pytest.mark.class_name('affected.folders.AffectedFolder')
+def test_load_folder(mocker_file_read_lines, mocker_keyed_class):
     """test load_folders reads the file only once per-refresh,
      and correctly constructs the folders list"""
+    next_key.key = 0
     folders1 = load_folders()
-    mocker_affected_folder.assert_has_calls([call('line1\n'), call('line2\n'), call('line3\n')])
-    assert mocker_affected_folder.call_count == 3
+    mocker_keyed_class.assert_has_calls([call('line1\n'), call('line2\n'), call('line3\n')])
+    assert mocker_keyed_class.call_count == 3
     folders2 = load_folders()
-    assert mocker_affected_folder.call_count == 3
+    assert mocker_keyed_class.call_count == 3
     assert folders1 == folders2
     assert list(folders1.keys()) == [1, 2, 3]
     folders3 = load_folders(True)
-    mocker_affected_folder.assert_has_calls([call('line1\n'), call('line2\n'), call('line3\n')])
-    assert mocker_affected_folder.call_count == 6
+    mocker_keyed_class.assert_has_calls([call('line1\n'), call('line2\n'), call('line3\n')])
+    assert mocker_keyed_class.call_count == 6
     assert folders1 != folders3
     assert list(folders3.keys()) == [4, 5, 6]
 
